263e0f1d04eb629148c240963fcaa047ded8b006
lvainio <lvainio@kth.se>
2024-09-25 16:36:59 +0200

    fix: fix typo

--- gosec/step1/foreground.sh
+++ gosec/step1/foreground.sh
@@ -1,6 +1,6 @@
 echo "Setting up the environment..."
 wget https://golang.org/dl/go1.21.0.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz.1
+sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 git clone https://github.com/lvainio/executable-tutorial-dd2482.git
 cd executable-tutorial-dd2482/programs
