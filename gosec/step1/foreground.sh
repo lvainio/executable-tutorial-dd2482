71f3367b9308cff9d68a90802923c67060decd56
lvainio <lvainio@kth.se>
2024-09-25 20:39:52 +0200

    fix: faulty path

--- gosec/step1/foreground.sh
+++ gosec/step1/foreground.sh
@@ -4,5 +4,5 @@ sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:~/go/bin
 git clone https://github.com/lvainio/executable-tutorial-dd2482.git
-cd /executable-tutorial-dd2482/programs
+cd executable-tutorial-dd2482/programs
 echo DONE
