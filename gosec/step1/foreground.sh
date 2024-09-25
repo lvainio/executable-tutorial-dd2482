ff8437b6164340b3f6bd75df3a024416900e5697
lvainio <lvainio@kth.se>
2024-09-25 16:50:07 +0200

    fix: update go version

--- gosec/step1/foreground.sh
+++ gosec/step1/foreground.sh
@@ -1,6 +1,6 @@
 echo "Setting up the environment..."
-wget https://golang.org/dl/go1.22.7.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.22.7.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.21.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:~/go/bin
 git clone https://github.com/lvainio/executable-tutorial-dd2482.git
