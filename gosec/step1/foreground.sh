00bdc7b81e7fa2c6c2709cafc953567088649f7e
lvainio <lvainio@kth.se>
2024-09-25 20:15:01 +0200

    fix: fix path

--- gosec/step1/foreground.sh
+++ gosec/step1/foreground.sh
@@ -3,6 +3,7 @@ wget https://golang.org/dl/go1.21.0.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:~/go/bin
+cd /executable-tutorial-dd2482/programs
 git clone https://github.com/lvainio/executable-tutorial-dd2482.git
-cd executable-tutorial-dd2482/programs
+cd /executable-tutorial-dd2482/programs
 echo DONE
